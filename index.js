54e246d0c5d00ee51f6607c4c7f1ea2030991b09
blackscure <wekesabuyahi@gmail.com>
2018-01-03 09:04:02 +0300

    my first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var msg ="hello javascrpt";
+console.log(msg);
+
+var resultsDiv =document.getElementById("results");
+reesultsDiv.innerHTML = "<p>This is from javascript</p>";
+
+console. log("msg is" + typeof(msg));
+console. log("resultsDiv "  + typeof(resultsDiv));
+
+var truefalse = true;
+console.log("truefalse is" + typeof(truefalse));
+
+var truefalse = ("truefalse is" + typeof(truefalse));
+
+if(!none) {
+    console.log("none is undefined")
+}  
